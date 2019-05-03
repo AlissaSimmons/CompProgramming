b24547d8a1e8ef5a32e26b2983a328ae9ed7e863
AlissaSimmons <42811844+AlissaSimmons@users.noreply.github.com>
2019-05-03 09:01:51 -0600

    Add files via upload

---
+++ PizzaParty.py
@@ -0,0 +1,9 @@
+import math
+import pygame
+# (Math.PI * length/2)^2
+# Area of the square - area of the circle
+
+#variables
+length
+area of a circle
+area of a square
---
+++ Rabbits.py
@@ -0,0 +1,8 @@
+#import statements
+import os 
+import pygame
+# Beginning Values
+months = 1
+adults = 2
+children = 0
+
